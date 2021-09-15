d320119d658ef727f712be401b8ee487b30fc3e1
Sachin Bumtariya <90308295+SachinBumtariya@users.noreply.github.com>
2021-09-15 12:54:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
-import { PureComponent } from "react";
+// import { PureComponent } from "react";
 import "./App.css";
-import Form from "./Components/Form";
-import FragmentDemo from "./Components/FragmentDemo";
-import LifeCycleA from "./Components/LifeCycleA";
-import ParentComp from "./Components/ParentComp";
-import Table from "./Components/Table";
+// import FocusInput from "./Components/FocusInput";
+import FRParentInput from "./Components/FRParentInput";
+// import Form from "./Components/Form";
+// import FragmentDemo from "./Components/FragmentDemo";
+// import LifeCycleA from "./Components/LifeCycleA";
+// import ParentComp from "./Components/ParentComp";
+// import RefsDemo from "./Components/RefsDemo";
+// import Table from "./Components/Table";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Form />
+        {/* <Form />
         <LifeCycleA />
         <FragmentDemo />
         <Table />
         <PureComponent />
-        <ParentComp />
+        <ParentComp /> */}
+        {/* <RefsDemo/> */}
+        {/* <FocusInput /> */}
+        <FRParentInput/>
       </div>
     );
   }
