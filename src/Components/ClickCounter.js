dd77c1ada6d570ea464229cea320fd0d0fecdca7
Sachin Bumtariya <90308295+SachinBumtariya@users.noreply.github.com>
2021-09-15 16:57:43 +0530

    Add files via upload

---
+++ src/Components/ClickCounter.js
@@ -0,0 +1,12 @@
+//higher order component
+import React, { Component } from "react";
+import withCounter from "./withCounter";
+
+class ClickCounter extends Component {
+  render() {
+    const { count, incrementCount } = this.props;
+    return <button onClick={incrementCount}>Clicked {count} times</button>;
+  }
+}
+
+export default withCounter(ClickCounter);
