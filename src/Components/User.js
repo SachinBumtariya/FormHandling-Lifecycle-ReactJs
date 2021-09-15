5199c2f4f19bdb6adcfe43138b4a8b2ec77f6174
Sachin Bumtariya <90308295+SachinBumtariya@users.noreply.github.com>
2021-09-15 18:17:02 +0530

    Add files via upload

---
+++ src/Components/User.js
@@ -0,0 +1,14 @@
+//Render Props
+import React, { Component } from 'react'
+
+class User extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.render(false)}
+            </div>
+        )
+    }
+}
+
+export default User
